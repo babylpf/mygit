45ebd4c3577b57b8c27ab6a009ee54f5f60a532e
lpfei <1547559508@qq.com>
2021-08-20 20:54:58 +0800

    修改Test01.java

--- src/test/java/org/example/Test01.java
+++ src/test/java/org/example/Test01.java
@@ -4,6 +4,8 @@ public class Test01 {
 
     public static void main(String[] args){
         System.out.println("test01...");
+        System.out.println("test02...");
+        System.out.println("test03...");
     }
 
 }
